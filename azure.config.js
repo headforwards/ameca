c73ab7b70688fb92db7d2c5a89814cd0edafd2e1
paul donovan <paul.donovan@headforwards.com>
2025-02-05 14:53:18 +0000

    rename the azure config to be svelte config

---
+++ azure.config.js
@@ -0,0 +1,24 @@
+import { vitePreprocess } from 'svelte-adapter-azure-swa';
+
+let adapter;
+if (process.env.GITHUB_ACTIONS) {
+    adapter = require('svelte-adapter-azure-swa').default;
+} else {
+    adapter = require('@sveltejs/adapter-auto').default;
+}
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	}
+};
+
+export default config;

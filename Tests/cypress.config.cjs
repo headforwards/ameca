2f03bd7275d61e2dac3e662b729c58da9478c21b
Sam <Sam.Perry@cornwall.gov.uk>
2025-02-11 10:51:33 +0000

    update base url

--- Tests/cypress.config.cjs
+++ Tests/cypress.config.cjs
@@ -2,6 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 	e2e: {
-		baseUrl: "https://white-mud-007637c03.4.azurestaticapps.net"
+		baseUrl: "http://workshop.headforwards.com/"
 	}
 });
